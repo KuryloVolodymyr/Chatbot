db4b478d21c5ca0f80d26385e64a59d1ebae48a6
Kurylo V <kurylo.volodymyr@gmail.com>
2018-03-21 14:48:39 +0200

    initial

---
+++ src/main/java/Bot/Controller/MainController.java
@@ -0,0 +1,52 @@
+package Bot.Controller;
+
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/callback")
+public class MainController {
+    private final String appSecret;
+    private final String verifyToken;
+    private final String pageAccessToken;
+
+
+    public MainController(@Value("${appSecret}") final String appSecret,
+                          @Value("${verifyToken}") final String verifyToken,
+                          @Value("@{pageAccessToken}") final String pageAccessToken){
+        this.appSecret = appSecret;
+        this.verifyToken = verifyToken;
+        this.pageAccessToken = pageAccessToken;
+    }
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<String> verifyWebhook(@RequestParam("hub.verify_token") final String token,
+                                                @RequestParam("hub.challenge") final String challenge,
+                                                @RequestParam("hub.mode") final String mode) {
+        System.out.println(token.equals(verifyToken));
+        System.out.println(challenge.equals("CHALLENGE_ACCEPTED"));
+        System.out.println(mode.equals("subscribe"));
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<Void> bodyTest(@RequestBody final String info){
+        JSONObject jsonObject = new JSONObject(info);
+        System.out.println(info);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+//    public ResponseEntity handleMessage(){
+//
+//    }
+//
+//    public ResponseEntity callSendAPI(){
+//
+//    }
+
+
+
+}
